c64ae7bc022fb41a4b679da788eecba7e658a388
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2023-09-11 14:49:08 +0200

    corrected json

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors({origin: 'http://127.0.0.1:5173',credentials: true}));
+app.use(cors({origin: '*',credentials: true}));
 
 // Import and use the API routes
 app.use('/api',router);
